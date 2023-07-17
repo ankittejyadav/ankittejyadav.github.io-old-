7c7f5c8b98ea35aaf8e0f3952e2101d75f7687d1
ankittejyadav <ankittejyadav@gmail.com>
2023-07-17 10:44:24 -0400

    final push

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "Business Sales Insight",
+    subtitle: "Data Warehouse and Visualization",
+    description:
+      "Implemented a Data Warehouse, performed ETL operations and created dashboards.",
+    image: "./project-1.gif",
+    link: "https://github.com/ankittejyadav/Sales-Insights-Tableau",
+  },
+  {
+    title: "Quantum Canvas",
+    subtitle: "Quantum",
+    description:
+      "Generative art on IonQ's Quantum hardware",
+    image: "./project-2.gif",
+    link: "https://github.com/ankittejyadav/Schrodingers-Ket",
+  },
+  {
+    title: "Simple Web App",
+    subtitle: "Simple App",
+    description:
+      "Demo website created for HackHarvard",
+    image: "./project-3.gif",
+    link: "https://github.com/ankittejyadav/Hi-Name",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "Web3",
+    description:
+      "Blockchain based aplication",
+    image: "./project-4.gif",
+    link: "https://github.com/ankittejyadav/Ecrypto",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I supervised Ankit at CARB-X for 10 months. During this time, Ankit supported a range of projects for our communications, data and project management teams. He always exhibited a positive attitude, and a willingness to troubleshoot and work independently while meeting project deadlines.",
+    // image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Robin Berghaus",
+    company: "CARB-X",
+  },
+  {
+    quote:
+      "Ankit's knowledge in Data Science, Python, and Pandas paved the way until the end. Ankit managed to solve complicated problems in professional way. One of the biggest challenge was when he was tasked to build the workflow for containerizing our codebase in Docker. Ankit tackled this challenge with determination and was able to successfully build the workflow for containerizing our codebase in Docker.",
+    // image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Faza Hikmatullah",
+    company: "All in Energy",
+  },
+  {
+    quote:
+      "Ankit was able to successfully implement other new skills for our team by setting up a cloud data pipeline using AWS S3, AWS Glue, AWS Athena, and AWS Quicksight, once again reading documentation, implementing different methods, and sharing the best method with the team during team meetings. After setting up that pipeline, he took lead on designing and implementing the foundation of our analytical dashboard to track the growth of our product using the BI AWS tool Quicksight. Ankit was able to contribute to the team on a strong technical level.",
+    // image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Joshua Minuskin",
+    company: "Pre Health",
+  },
+];
+
+export const skills = [
+  "Python",
+  'SQL',
+  "React",
+  "JavaScript",
+  "Node",
+  "Java",
+];
